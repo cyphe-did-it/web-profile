0db95675fe1123aee2e252055d3e26ee741d78fc
cyphe-did-it <135584697+cyphe-did-it@users.noreply.github.com>
2024-12-19 23:59:58 +0800

    fixed the missing dependency

--- app/components/TypedName.tsx
+++ app/components/TypedName.tsx
@@ -5,33 +5,29 @@ interface Props {
   fullText: string;
 }
 
-const TypedText: React.FC<Props> = ({fullText}) => {
+const TypedText: React.FC<Props> = ({ fullText }) => {
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
-  // const fullText = 'CYPHIL RAVEN MIDSAPAK';
   const typingSpeed = 150; // Speed in milliseconds
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setText((prev) => prev + fullText[index]);
-      setIndex((prev) => prev + 1);
+    if (index < fullText.length) {
+      const interval = setInterval(() => {
+        setText((prev) => prev + fullText[index]);
+        setIndex((prev) => prev + 1);
+      }, typingSpeed);
 
-      if (index === fullText.length - 1) {
-        setTimeout(() => {
-          setText(''); // Clear text
-          setIndex(0); // Restart typing
-        }, 50); // Add a delay before restarting
-      }
-    }, typingSpeed);
+      return () => clearInterval(interval); // Cleanup interval on unmount or when index changes
+    } else {
+      // Restart typing after completing the text
+      setTimeout(() => {
+        setText(''); // Clear text
+        setIndex(0); // Restart typing
+      }, 1000); // Add a delay before restarting
+    }
+  }, [index, fullText]); // Add 'index' and 'fullText' as dependencies
 
-    return () => clearInterval(interval); // Cleanup interval on unmount
-  }, [index]);
-
-  return (
-    <>
-      <h1>{text}</h1>
-    </>
-  );
+  return <h1>{text}</h1>;
 };
 
 export default TypedText;
